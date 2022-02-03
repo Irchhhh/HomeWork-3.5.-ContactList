fe82823a77079e48e38b4d574ee18bee4fdaba73
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-02-03 15:42:29 +0300

    Initial Commit

---
+++ Home Work 3.5. Lists/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Home Work 3.5. Lists
+//
+//  Created by Ирина on 03.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Home Work 3.5. Lists/Home_Work_3_5__ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Work_3_5__ListsApp.swift
+//  Home Work 3.5. Lists
+//
+//  Created by Ирина on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Home_Work_3_5__ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
